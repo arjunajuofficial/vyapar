8f5ce13f281f5a357bc5b72b9ae579c0a1cdde9a
Immanuel-john <imjjohn18@gmail.com>
2018-03-10 22:38:06 +0530

    second commit

--- UI/src/app/productdetails/productdetails.component.ts
+++ UI/src/app/productdetails/productdetails.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Http} from '@angular/http';
+import * as io from "socket.io-client";
 @Component({
   selector: 'app-productdetails',
   templateUrl: './productdetails.component.html',
   styleUrls: ['./productdetails.component.css']
 })
 export class ProductdetailsComponent implements OnInit {
-
-  constructor() { }
-
+  socket = io('http://localhost:8000');
+  constructor(private http:Http) { }
+  rows=[];
   ngOnInit() {
+    this.socket.on('address', function (data) {
+    console.log(data);
+    this.load();
+  }.bind(this));
+  this.load();
+  }
+  load=function(){
+    this.http.get('http://localhost:8000/load').subscribe(data => {
+      // Read the result field from the JSON response.
+      this.rows=JSON.parse(data["_body"]);
+    });
   }
 
+
+  insert=function(){
+    const body={code:this.code,name:this.name1,price:this.price,gst:this.gst};
+    this.http.post('http://localhost:8000/addproduct',body).subscribe();
+  }
+  update=function(product){
+    const body={code:product.product_code,name:product.product_name,price:product.product_price,gst:product.product_gst};
+    this.http.post('http://localhost:8000/update',body).subscribe();
+  }
 }
